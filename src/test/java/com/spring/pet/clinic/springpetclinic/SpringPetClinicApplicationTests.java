6292fdfac63799e2af64fed878f873fdb91d9182
sreeraj <sree.valapil@gmail.com>
2020-04-29 15:51:00 +0200

    good

---
+++ src/test/java/com/spring/pet/clinic/springpetclinic/SpringPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.pet.clinic.springpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
